d5ce92bfbc60bb40e18b15253636e5149fd4441e
Carlos M Jimenez <developer@cmjimenez.com>
2020-01-04 23:13:05 -0500

    add nodemon

--- Shareable-playlist-api/src/controllers/index.js
+++ Shareable-playlist-api/src/controllers/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',function(req,res){
-    res.send("I am working");
-})
+router.get('/*', function(req,res){
+    res.send("Welcome to Shareable Playlists");
+});
 
 module.exports = router
