2dff2a7738b8b5189f5008f40c7e4264c1de4711
Carlos M Jimenez <developer@cmjimenez.com>
2020-01-14 07:12:20 -0500

    start adding test for spotify auth

--- Shareable-playlist-api/src/service/authorization/spotify-auth.test.js
+++ Shareable-playlist-api/src/service/authorization/spotify-auth.test.js
@@ -1,3 +1,18 @@
-test('asyncHandleSpotifyAuthorizationCallback', ()=> {
-  expect(true).toBeFalsy();
+import spotifyAuth from './spotify-auth';
+
+describe('getSpotifyAuthorizationUri', ()=>{
+  it('Returns the redirect url with reqired query parameters', ()=>{
+    const uri = spotifyAuth.getSpotifyAuthorizationUri();
+    const searchParameters = uri.searchParams;
+    const clientId = searchParameters.get('client_id');
+    const responseType = searchParameters.get('response_type');
+    const redirectUri = searchParameters.get('redirect_uri');
+
+    expect(clientId).toBeDefined();
+    expect(responseType).toBeDefined();
+    expect(redirectUri).toBeDefined();
+  });
+});
+describe('asyncHandleSpotifyCallback', ()=>{
+
 });
