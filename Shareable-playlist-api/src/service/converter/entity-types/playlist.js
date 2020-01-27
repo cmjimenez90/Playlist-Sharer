d2741c0e5e1b60cc6beed26bf2e220da7796765a
Carlos M Jimenez <developer@cmjimenez.com>
2020-01-26 20:27:19 -0500

    change playlist to have default songs array

--- Shareable-playlist-api/src/service/converter/entity-types/playlist.js
+++ Shareable-playlist-api/src/service/converter/entity-types/playlist.js
@@ -1,7 +1,7 @@
 'use strict';
 
 export default class Playlist {
-  constructor(playlistName, songs) {
+  constructor(playlistName, songs = []) {
     this.name =playlistName;
     this.songs = songs;
   }
