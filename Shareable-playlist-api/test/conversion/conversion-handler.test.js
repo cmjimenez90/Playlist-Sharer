284b642719ff6fc3e0ddae4e2df46462062ccc99
Carlos M Jimenez <developer@cmjimenez.com>
2020-01-30 07:05:43 -0500

    start test design for ConversionHandler convert method

--- Shareable-playlist-api/test/conversion/conversion-handler.test.js
+++ Shareable-playlist-api/test/conversion/conversion-handler.test.js
@@ -1,3 +1,11 @@
+import ConversionHandler from '../../src/service/conversion/conversion-handler';
+import ProviderURL from '../../src/service/provider/base/provider-url';
+
 describe('ConversionHandler', () => {
-  it('this is a test', () => {});
+  describe('Convert', () => {
+    it('Converts a ProviderUrl to desired format', async () => {
+      const conversionHandler = new ConversionHandler();
+      const convertedProvider = conversionHandler.convert(providerURL, format);
+    });
+  });
 });
