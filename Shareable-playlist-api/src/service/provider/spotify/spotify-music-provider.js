df07486f136c39201c456424816232a92cfbf454
Carlos M Jimenez <developer@cmjimenez.com>
2020-02-01 23:17:04 -0500

    Fix constructor bug in SpotifyMusicProvider

--- Shareable-playlist-api/src/service/provider/spotify/spotify-music-provider.js
+++ Shareable-playlist-api/src/service/provider/spotify/spotify-music-provider.js
@@ -2,6 +2,6 @@ import Provider from '../base/provider';
 import SpotifyUrlParser from './spotify-url-parser';
 export default class SpotifyMusicProvider extends Provider {
   constructor() {
-    super('apple', 'api.spotify.com', 'open.spotify.com', new SpotifyUrlParser());
+    super('spotify', 'api.spotify.com', 'open.spotify.com', new SpotifyUrlParser());
   }
 };
