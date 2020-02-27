f36379164a35080600187fe56b916bead83e145f
Carlos M Jimenez <developer@cmjimenez.com>
2020-02-27 07:11:08 -0500

    implement apple song converter
    add test for song converter

--- Shareable-playlist-api/src/service/provider/apple/apple-music-converter.js
+++ Shareable-playlist-api/src/service/provider/apple/apple-music-converter.js
@@ -1,3 +1,4 @@
+import Song from '../../provider/types/song';
 import ProviderConverter from '../../provider/base/provider-converter';
 
 export default class AppleMusicConverter extends ProviderConverter {
@@ -7,13 +8,25 @@ export default class AppleMusicConverter extends ProviderConverter {
   };
 
   async asyncConvertAlbum(album) {
-    const searchTerm = `${album.name}+${album.artist}`;
 
-    this.appleMusicClient.asyncSearch([albums], searchTerm, 'US');
   };
 
   async asyncConvertSong(song) {
-
+    let searchTerm = `${song.name}+${song.artist}+${song.releaseAlbum}`;
+    searchTerm = searchTerm.replace(' ', '+');
+    try {
+      const response = await this.appleMusicClient.asyncSearch(['songs'], searchTerm);
+      const matchingSongs = response.results.songs.data;
+      const matched = matchingSongs.filter((item)=> {
+        const attributes = item.attributes;
+        return (attributes.name == song.name);
+      });
+      const convertedSong = new Song(matched[0].attributes.name, matched[0].attributes.artistName, matched[0].attributes.albumName, matched[0].attributes.url);
+      console.log(convertedSong);
+      return convertedSong;
+    } catch (error) {
+      return error;
+    }
   };
 
   async asyncConvertPlaylist(userAccessToken, playlist) {
