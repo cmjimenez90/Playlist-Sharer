8f99cf44b979ba34d26677fdc5fe9c490fe41715
= <=>
2020-03-15 19:39:02 -0400

    remove all implementation and start over

--- ShareablePlaylist-Mobile-Applicaiton/App.js
+++ ShareablePlaylist-Mobile-Applicaiton/App.js
@@ -1,21 +1,28 @@
-import {styles} from './style/main.style'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import {Text, View, StatusBar} from 'react-native';
-import UrlDisplay from './components/url-display';
-import ConvertButton from './components/convert-button';
+
+import {color} from './style/main.style';
+import SignIn from './authentication/SignIn';
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return(
-    <View style={styles.container}> 
-      <View style={styles.statusBar}>
-        <StatusBar />
-      </View>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Shareable Playlists</Text>
-      </View>
-      <View style={styles.body}>
-        <UrlDisplay url="https://music.apple.com/us/album/i-love-me/1501001885?i=1501002276"></UrlDisplay>
-        <ConvertButton url="https://music.apple.com/us/album/i-love-me/1501001885?i=1501002276"></ConvertButton>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: color.secondary,
+        },
+        headerTintColor: color.accent,
+        headerTitleAlign: "left",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+        title: 'Playlist Sharer',
+      }}>
+        <Stack.Screen name='SignIn' component={SignIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
 )}
 
--- ShareablePlaylist-Mobile-Applicaiton/style/main.style.js
+++ ShareablePlaylist-Mobile-Applicaiton/style/main.style.js
@@ -6,62 +6,9 @@ export const color = {
 }
 
 export const styles = StyleSheet.create({
-    statusBar: {
-        height: (Platform.OS === 'ios') ? 40 : StatusBar.currentHeight,
-        backgroundColor: color.secondary
-
-    },
     container: {
         height: "100%",
         backgroundColor: color.primary
-    },
-    header: {
-        backgroundColor: color.secondary,
-        height: 60,
-        justifyContent: "center"
-    },
-    headerText: {
-        paddingLeft: 20,
-        color: color.accent,
-        fontWeight: "bold",
-        fontSize: 30,
-    },
-    body: {
-        height: "80%",
-        justifyContent: "center",
-    },
-    urlDisplayContainer: {
-        alignItems: "center",
-        marginBottom: 100
-    },
-    urlDisplayTextContainer: {
-        marginBottom: 10,
-    },
-    urlDisplay: {
-        width: 200,
-        height: 200,
-    },
-    urlDisplayName: {
-        color: color.accent,
-        textAlign: "right",
-        fontSize: 30
-    },
-    urlDisplayType:{
-        color: color.accent,
-        textAlign: "right",
-        fontSize: 20,
-        fontWeight: "bold",
-        textTransform: "uppercase"
-    },
-    logoButtonContainer: {
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    logoButton: {
-       width: 200,
-       height: 50,
-       resizeMode: "contain",
-       marginBottom: 20
     }
   });
   
