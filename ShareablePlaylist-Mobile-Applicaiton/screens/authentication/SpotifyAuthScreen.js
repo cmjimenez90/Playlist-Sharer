4acb173a6cf8f3230c04f482044cfed3a213ae1e
= <=>
2020-03-19 16:25:34 -0400

    spotify dirty authorization created

--- ShareablePlaylist-Mobile-Applicaiton/screens/authentication/SpotifyAuthScreen.js
+++ ShareablePlaylist-Mobile-Applicaiton/screens/authentication/SpotifyAuthScreen.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { WebView } from 'react-native-webview';
+import { Alert } from 'react-native';
 
-const SpotifyAuthScreen = () => {
+import {AuthenticationContext} from '../../authentication/AuthenticationContext';
 
-    let webView = null;
+const SpotifyAuthScreen = ({navigation}) => {
+
+   const [state,action] = useContext(AuthenticationContext);
+   let webView = null;
 
    const handleAuthorizationNavigation = (newNav) => {
         const {url,loading} = newNav;
@@ -12,14 +16,22 @@ const SpotifyAuthScreen = () => {
         };
     };
 
-    const handlerAuthorizationResponse = (event) => {
+    const handleAuthorizationResponse = (event) => {
         const data = event.nativeEvent.data;
         const response = JSON.parse(data);
-        console.log(response);
+        if(response.hasError){
+            Alert.alert('Authorization was not granted successfully, please try again..');
+            navigation.navigate('SignInScreen');
+        }
+        else {
+            action({type: 'AuthorizeSpotify',payload: response.authorizationToken});
+            navigation.navigate('MainScreen')
+        }
+
     };
-    
+
     return (
-        <WebView ref={ref => (webView = ref)} source={{uri: 'http://localhost:3000/authorize/spotify'}} onNavigationStateChange={handleAuthorizationNavigation} onMessage={handlerAuthorizationResponse} />
+        <WebView ref={ref => (webView = ref)} source={{uri: 'http://localhost:3000/authorize/spotify'}} onNavigationStateChange={handleAuthorizationNavigation} onMessage={handleAuthorizationResponse} />
     )
 }
 
