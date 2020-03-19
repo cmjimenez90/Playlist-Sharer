54332177d0d353d6d4019665c6719160feb1f73f
= <=>
2020-03-19 16:26:35 -0400

    remove unneccessary import

--- ShareablePlaylist-Mobile-Applicaiton/authentication/useAuthentication.js
+++ ShareablePlaylist-Mobile-Applicaiton/authentication/useAuthentication.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react'
+import {useContext} from 'react'
 import {AuthenticationContext} from './AuthenticationContext';
 
 export default useAuthentication = () => {
